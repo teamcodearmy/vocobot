e52b856666458784e4fb52fba4596bfd11050315
teamcodearmy <54304102+teamcodearmy@users.noreply.github.com>
2020-02-27 10:39:09 +0500

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 
 clientid="7f5486f78731e761c99878e8e37d540f5868a99c12a16089be227b372a366c36"
 consumer_secrete="efa34f8ae8542f4de35ce0a4b7108421e3d32a6bd9ae8eecc0d8efcb6ff29d7a"
-calbackuri="https://vocobot.herokuapp.com/authorize"
+calbackuri="https://vocobot.herokuapp.com/"
 
 auth = WithingsAuth(
     client_id=clientid,
