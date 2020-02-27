7febad07001b69ed1dbcdaaa8b7207a222a94f1a
teamcodearmy <54304102+teamcodearmy@users.noreply.github.com>
2020-02-27 08:59:00 +0500

    chnage

--- app.py
+++ app.py
@@ -7,10 +7,25 @@ app=Flask(__name__)
 
 clientid="7f5486f78731e761c99878e8e37d540f5868a99c12a16089be227b372a366c36"
 consumer_secrete="efa34f8ae8542f4de35ce0a4b7108421e3d32a6bd9ae8eecc0d8efcb6ff29d7a"
+calbackuri="https://vocobot.herokuapp.com/"
+
+auth = WithingsAuth(
+    client_id=clientid,
+    consumer_secret=consumer_secrete,
+    callback_uri=calbackuri,
+    mode='demo',
+    scope=(
+        AuthScope.USER_ACTIVITY,
+        AuthScope.USER_METRICS,
+        AuthScope.USER_INFO,
+        AuthScope.USER_SLEEP_EVENTS,
+    )
+)
 
 @app.route("/",methods=["POST","GET"])
 def index():
-    return "helo world"
+    authorize_url = auth.get_authorize_url()
+    return "helo world"+authorize_url
 
 if __name__=="__main__":
     app.run()
