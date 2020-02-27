c320480e016bacd41b472154cab2c65562d21507
teamcodearmy <54304102+teamcodearmy@users.noreply.github.com>
2020-02-27 08:49:55 +0500

    update chnage

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from withings_api import WithingsAuth, WithingsApi, AuthScope
+from withings_api.common import get_measure_value,MeasureType
+from urllib import parse
+
 app=Flask(__name__)
+
+clientid="7f5486f78731e761c99878e8e37d540f5868a99c12a16089be227b372a366c36"
+consumer_secrete="efa34f8ae8542f4de35ce0a4b7108421e3d32a6bd9ae8eecc0d8efcb6ff29d7a"
+
 @app.route("/",methods=["POST","GET"])
 def index():
     return "helo world"
